c51b9077e814364530f1fc10a00be6844467f94b
bagdadiemre <bagdadiemre@gmail.com>
2023-07-20 12:14:50 +0300

    nextjs init with typescript from beginning

--- react-apps/react-next/prisma/migrations/20230720084301_init/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "Todo" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "title" TEXT NOT NULL,
-    "completed" BOOLEAN,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
