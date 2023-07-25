ec6b3664544e103958c752735cfa8c97d6116f30
bagdadiemre <bagdadiemre@gmail.com>
2023-07-25 23:45:44 +0300

    PokemonDetail component create

--- week2/pokedex-app/src/components/index.ts
+++ week2/pokedex-app/src/components/index.ts
@@ -1,5 +1,7 @@
 import PokemonList from "./PokemonList";
 import PokemonCard from "./PokemonCard";
+import PokemonDeatil from "./PokemonDetail";
 
 export { PokemonList };
 export { PokemonCard };
+export { PokemonDeatil };
