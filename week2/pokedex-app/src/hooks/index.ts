81c922b8b2a48d31d72718d8b01e0dd307749101
bagdadiemre <bagdadiemre@gmail.com>
2023-07-25 13:49:57 +0300

    fetch pokemons

---
+++ week2/pokedex-app/src/hooks/index.ts
@@ -0,0 +1,3 @@
+import usePokemons from "./usePokemon";
+
+export { usePokemons };
--- week2/pokedex-app/src/hooks/usePokemon.ts
+++ week2/pokedex-app/src/hooks/usePokemon.ts
@@ -1,8 +1,23 @@
 import { useState } from "react";
-import { IndexedPokemon } from "../interface/pokemon.interface";
+import {
+  IndexedPokemon,
+  PokemonListResponse,
+} from "../interface/pokemon.interface";
+import { httpCliet } from "../api/httpClient";
+import { POKEMON_API_POKEMON_URL } from "../constant";
 
 const usePokemons = () => {
   const [pokemons, setPokemons] = useState<IndexedPokemon[]>([]);
+  const [nextUrl, setNextUrl] = useState<string | null>(
+    POKEMON_API_POKEMON_URL
+  );
+
+  const fetchPokemon = async () => {
+    if (nextUrl) {
+      const result = await httpCliet.get<PokemonListResponse>(nextUrl);
+      console.log(result);
+    }
+  };
 
   return {
     pokemons,
