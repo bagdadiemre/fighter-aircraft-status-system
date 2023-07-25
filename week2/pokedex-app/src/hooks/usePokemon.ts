1ae7064ce04d835b55c6896a498aab5f5877717e
bagdadiemre <bagdadiemre@gmail.com>
2023-07-26 00:32:38 +0300

    custom hook usePokemon create

--- week2/pokedex-app/src/hooks/usePokemon.ts
+++ week2/pokedex-app/src/hooks/usePokemon.ts
@@ -1,9 +1,38 @@
+import { useState } from "react";
+import { DetailPokemon } from "../interface";
+import { httpClient } from "../api";
+import { POKEMON_API_POKEMON_URL } from "../constants";
+
 interface UsePokemonProps {
   pokemonName: string | undefined;
 }
 
 const usePokemon = ({ pokemonName }: UsePokemonProps) => {
-  return {};
+  const [pokemon, setPokemon] = useState<DetailPokemon | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    if (pokemonName) {
+      fetchPokemon();
+    }
+  }, [pokemonName]);
+
+  const fetchPokemon = async () => {
+    if (pokemonName) {
+      setIsLoading(true);
+      const url = `${POKEMON_API_POKEMON_URL}/${pokemonName}`;
+      const result = await httpClient.get<DetailPokemon>(url);
+      if (result?.data) {
+        setPokemon(result.data);
+      }
+      setIsLoading(false);
+    }
+  };
+
+  return {
+    pokemon,
+    isLoading,
+  };
 };
 
 export default usePokemon;
