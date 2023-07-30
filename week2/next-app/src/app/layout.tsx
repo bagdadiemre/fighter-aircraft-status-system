b03bd3b6ae1a059cc963db00caadf800b5f7263d
bagdadiemre <bagdadiemre@gmail.com>
2023-07-30 20:42:51 +0300

    project1 backend cloned

--- week2/next-app/src/app/layout.tsx
+++ week2/next-app/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import Link from "next/link"
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,7 +20,6 @@ export default function RootLayout({
       <body className={inter.className}>
         <Link href="/">Home</Link>
         <Link href="/pokemons">Pokemons</Link>
-
 
         {children}
       </body>
