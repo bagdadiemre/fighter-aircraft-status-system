81886e16c929695126cbc3efcabd712f190117da
bagdadiemre <bagdadiemre@gmail.com>
2023-07-31 15:47:40 +0300

    clear2

--- projects/project1/frontend/src/App.js
+++ projects/project1/frontend/src/App.js
@@ -1,28 +1,35 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import MainLayout from "./components/MainLayout";
-import LoginForm from "./components/Auth/LoginForm";
-import NotFound from "./components/Auth/NotFound";
-import ProtectedPage from "./components/Auth/ProtectedPage";
+// src/components/App.js
+import React, { useContext } from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+import { AuthContext } from "./contexts/AuthContext";
+import Login from "./components/Auth/Login";
+import Messages from "./Messages/Messages"; // You need to implement the Messages component
+import Users from "./Users/Users"; // You need to implement the Users component
+import Reports from "./Reports/Reports"; // You need to implement the Reports component
+import NotFound from "./Auth/NotFound";
+import NotAuthorized from "./Auth/NotAuthorized";
 
 const App = () => {
+  const { user } = useContext(AuthContext);
+
   return (
-    <Router>
-      <Switch>
-        {/* Public Route - Login Form */}
-        <Route exact path="/login" component={LoginForm} />
-
-        {/* Protected Route - Add more protected routes later */}
-        <Route exact path="/">
-          <MainLayout>
-            <Route exact path="/" component={ProtectedPage} />
-          </MainLayout>
-        </Route>
-
-        {/* 404 Not Found Page */}
-        <Route path="*" component={NotFound} />
-      </Switch>
-    </Router>
+    <Switch>
+      <Route path="/login" component={Login} />
+      {user ? (
+        <>
+          <Route path="/messages" component={Messages} />
+          {user.role === "admin" && <Route path="/users" component={Users} />}
+          {user.role === "admin" && (
+            <Route path="/reports" component={Reports} />
+          )}
+          <Redirect exact from="/" to="/messages" />
+          <Route path="/not-authorized" component={NotAuthorized} />
+          <Route component={NotFound} />
+        </>
+      ) : (
+        <Redirect to="/login" />
+      )}
+    </Switch>
   );
 };
 
--- projects/project1/frontend/src/index.js
+++ projects/project1/frontend/src/index.js
@@ -1,13 +1,17 @@
+// src/index.js
 import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import AuthProvider from "./contexts/AuthProvider";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import { AuthProvider } from "./contexts/AuthContext";
+import App from "./components/App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
-  </React.StrictMode>
+    <Router>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
