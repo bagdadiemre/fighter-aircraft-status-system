c1a7f2e2b3d5e6e01988a70a523d2826cb0b5773
bagdadiemre <bagdadiemre@gmail.com>
2023-07-31 15:23:44 +0300

    clear

--- projects/project1/frontend/src/App.js
+++ projects/project1/frontend/src/App.js
@@ -1,21 +1,28 @@
 import React from "react";
-import AuthProvider from "./contexts/AuthProvider";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import MainLayout from "./components/MainLayout";
 import LoginForm from "./components/Auth/LoginForm";
+import NotFound from "./components/Auth/NotFound";
+import ProtectedPage from "./components/Auth/ProtectedPage";
 
 const App = () => {
   return (
-    <AuthProvider>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "100vh",
-        }}
-      >
-        <LoginForm />
-      </div>
-    </AuthProvider>
+    <Router>
+      <Switch>
+        {/* Public Route - Login Form */}
+        <Route exact path="/login" component={LoginForm} />
+
+        {/* Protected Route - Add more protected routes later */}
+        <Route exact path="/">
+          <MainLayout>
+            <Route exact path="/" component={ProtectedPage} />
+          </MainLayout>
+        </Route>
+
+        {/* 404 Not Found Page */}
+        <Route path="*" component={NotFound} />
+      </Switch>
+    </Router>
   );
 };
 
