ae87bbea52b5b9f73e8093c24b1892544b2e4fa9
bagdadiemre <bagdadiemre@gmail.com>
2023-08-15 12:11:39 +0300

    theme toggle added to header

--- projects/project1/frontend/src/components/Common/Header.js
+++ projects/project1/frontend/src/components/Common/Header.js
@@ -1,8 +1,7 @@
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { logout } from "../../services/authApi";
 import {
-  AppBar,
   Button,
   Toolbar,
   Typography,
@@ -10,9 +9,14 @@ import {
   Menu,
   MenuItem,
 } from "@mui/material";
+import Brightness4Icon from "@mui/icons-material/Brightness4";
+import Brightness7Icon from "@mui/icons-material/Brightness7";
 
 const Header = ({ headerName, context }) => {
   const [anchorEl, setAnchorEl] = useState(null);
+  const [darkMode, setDarkMode] = useState(
+    localStorage.getItem("darkMode") === "true"
+  );
   const navigate = useNavigate();
 
   const handleMenuOpen = (event) => {
@@ -28,55 +32,71 @@ const Header = ({ headerName, context }) => {
     navigate("/login");
   };
 
+  const toggleDarkMode = () => {
+    const newDarkMode = !darkMode;
+    setDarkMode(newDarkMode);
+    localStorage.setItem("darkMode", newDarkMode);
+  };
+
+  useEffect(() => {
+    if (darkMode) {
+      document.body.classList.add("dark-mode");
+    } else {
+      document.body.classList.remove("dark-mode");
+    }
+  }, [darkMode]);
+
   return (
-    <AppBar
-      position="static"
+    <Toolbar
+      position="fixed"
       sx={{
+        backgroundColor: "#006d77",
         borderRadius: "20px",
-        marginBottom: "20px",
+        color: "white",
+        marginTop: "5px",
       }}
     >
-      <Toolbar sx={{ backgroundColor: "#006d77", borderRadius: "10px" }}>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          {headerName}
-        </Typography>
-        {context?.role === "admin" && (
-          <div>
-            {headerName !== "Messages Page" && (
-              <Button color="inherit" component={Link} to="/messages">
-                Messages
-              </Button>
-            )}
-            <Button color="inherit" component={Link} to="/users">
-              Users
-            </Button>
-            <Button color="inherit" component={Link} to="/reports">
-              Reports
-            </Button>
-          </div>
-        )}
+      <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+        {headerName}
+      </Typography>
+      {context?.role === "admin" && (
         <div>
-          <Button
-            color="inherit"
-            aria-controls="user-menu"
-            aria-haspopup="true"
-            onClick={handleMenuOpen}
-          >
-            <Avatar src={context?.base64Photo} alt={context?.username} />
+          <Button color="inherit" component={Link} to="/messages">
+            Messages
           </Button>
-          <Menu
-            id="user-menu"
-            anchorEl={anchorEl}
-            keepMounted
-            open={Boolean(anchorEl)}
-            onClose={handleMenuClose}
-          >
-            <MenuItem disabled>{context?.username}</MenuItem>
-            <MenuItem onClick={handleLogout}>Logout</MenuItem>
-          </Menu>
+          <Button color="inherit" component={Link} to="/users">
+            Users
+          </Button>
+          <Button color="inherit" component={Link} to="/reports">
+            Reports
+          </Button>
         </div>
-      </Toolbar>
-    </AppBar>
+      )}
+      <div>
+        <Button
+          color="inherit"
+          aria-controls="user-menu"
+          aria-haspopup="true"
+          onClick={handleMenuOpen}
+        >
+          <Avatar src={context?.base64Photo} alt={context?.username} />
+        </Button>
+        <Menu
+          id="user-menu"
+          anchorEl={anchorEl}
+          keepMounted
+          open={Boolean(anchorEl)}
+          onClose={handleMenuClose}
+        >
+          <MenuItem disabled>{context?.username}</MenuItem>
+          <MenuItem onClick={handleLogout}>Logout</MenuItem>
+          <MenuItem onClick={toggleDarkMode}>
+            {darkMode ? <Brightness7Icon /> : <Brightness4Icon />}
+            {darkMode ? "Light Mode" : "Dark Mode"}
+          </MenuItem>
+        </Menu>
+      </div>
+    </Toolbar>
   );
 };
 
