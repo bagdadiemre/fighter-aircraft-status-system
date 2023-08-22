388d118a80629327c14145ef15217626ed0e4d50
bagdadiemre <bagdadiemre@gmail.com>
2023-08-22 12:10:26 +0300

    copy frontend for strapi

---
+++ projects/project1/project1_frontend_strapi/src/components/ContactFormPage/index.js
@@ -0,0 +1,6 @@
+import CountrySelect from "./CountrySelect";
+import GenderRadioGroup from "./GenderRadioGroup";
+import MessageField from "./MessageField";
+import NameField from "./NameField";
+
+export { CountrySelect, GenderRadioGroup, MessageField, NameField };
---
+++ projects/project1/project1_frontend_strapi/src/components/index.js
@@ -0,0 +1,13 @@
+import Header from "./Header";
+import MessagesTable from "./MessagesTable";
+import MessagesInfiniteScrollTable from "./MessagesInfiniteScrollTable";
+import MessagesPaginatedTable from "./MessagesPaginatedTable";
+import UsersTable from "./UsersTable";
+
+export {
+  Header,
+  MessagesTable,
+  MessagesInfiniteScrollTable,
+  MessagesPaginatedTable,
+  UsersTable,
+};
