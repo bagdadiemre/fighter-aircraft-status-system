b54471b80be48914e3c8d55f223c01aaab637c9c
bagdadiemre <bagdadiemre@gmail.com>
2023-08-31 19:58:05 +0300

    clean

--- contact-form-management-system_react-nodejs/frontend/src/components/ContactFormPage/index.js
+++
@@ -1,6 +0,0 @@
-import CountrySelect from "./CountrySelect";
-import GenderRadioGroup from "./GenderRadioGroup";
-import MessageField from "./MessageField";
-import NameField from "./NameField";
-
-export { CountrySelect, GenderRadioGroup, MessageField, NameField };
--- contact-form-management-system_react-nodejs/frontend/src/contexts/AuthProvider.js
+++
@@ -1,5 +0,0 @@
-import { AuthContext } from "./AuthContext";
-
-export const AuthProvider = ({ children, value }) => (
-  <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
-);
